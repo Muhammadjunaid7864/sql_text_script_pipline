9320dfadc07556468b0973b5bb22977160551350
Muhammafjunaid7864 <muhammadjunaid104developer@gmail.com>
2023-11-06 00:29:58 +0500

    again

--- src/script/deadlockfile1.sql
+++ src/script/deadlockfile1.sql
@@ -1,20 +1,7 @@
 BEGIN TRAN;
 
 UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
-WAITFOR DELAY '00:00:10';
+WAITFOR DELAY '00:00:12';
 UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
-UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
-WAITFOR DELAY '00:00:10';
-UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
-UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
-WAITFOR DELAY '00:00:10';
-UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
-UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
-WAITFOR DELAY '00:00:10';
-UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
-UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
-WAITFOR DELAY '00:00:10';
-UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
-
 Go
 
