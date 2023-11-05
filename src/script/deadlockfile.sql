1828f0290e7f9fc9623eed0e353f6a2f5846061e
Muhammafjunaid7864 <muhammadjunaid104developer@gmail.com>
2023-11-05 20:55:10 +0500

    manay command

--- src/script/deadlockfile.sql
+++ src/script/deadlockfile.sql
@@ -2,5 +2,15 @@ BEGIN TRAN;
 WAITFOR DELAY '00:00:10';
 UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
 UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
+UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
+UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
+UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
+UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
+UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
+UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
+UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
+UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
+UPDATE Students SET FirstName = 'Nasir' WHERE StudentID = 1;
+UPDATE teacher SET FirstName = 'Nasir' WHERE teacherID = 1;
 
 Go
